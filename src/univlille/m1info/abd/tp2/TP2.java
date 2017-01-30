b47bc995cfe40eaaf6a3f92c8e90b177bee9697c
Caruyer Pierre <pierre.caruyer@etudiant.univ-lille1.fr>
2017-01-30 15:05:30 +0100

    fixed join junit test

--- src/univlille/m1info/abd/tp2/TP2.java
+++ src/univlille/m1info/abd/tp2/TP2.java
@@ -31,6 +31,7 @@ public class TP2 {
 		
 		projection = new SimpleDBRelation(new DefaultRelationSchema(projectionName, projectionSorts));
 		relation.switchToReadMode();
+		projection.switchToWriteMode();
 		
 		for(String[] tuple = relation.nextTuple(); tuple != null; tuple = relation.nextTuple()){
 			String[] projectionTuple = new String[attrNames.length];
@@ -59,6 +60,7 @@ public class TP2 {
 			return null;
 		
 		relation.switchToReadMode();
+		selectedRelation.switchToWriteMode();
 		
 		for(String[] tuple = relation.nextTuple(); tuple != null; tuple = relation.nextTuple())
 			if(tuple[attribute].equals(value))
@@ -69,7 +71,7 @@ public class TP2 {
 	}
 	
 	public String computeJoin(SimpleSGBD sgbd, String inputRelName1, String inputRelName2){
-		int index1 = -1, index2 = -1;
+		int index = -1;
 		SimpleDBRelation relation1 = sgbd.getRelation(inputRelName1), relation2 = sgbd.getRelation(inputRelName2);
 		String[] sorts1 = relation1.getRelationSchema().getSort(), sorts2 = relation2.getRelationSchema().getSort();
 		
@@ -78,41 +80,49 @@ public class TP2 {
 		 */
 		for(int i = 0; i < sorts1.length; i++)
 			for(int j = 0; j < sorts2.length; j++)
-				if(sorts1[i].equals(sorts2[j])){
-					index1 = i;
-					index2 = j;
-				}
+				if(sorts1[i].equals(sorts2[j]))
+					index = j;
 		
-		if(index1 < 0)
+		if(index < 0)
 			return null;
 		
 		/*
 		 * Associe à la relation de sortie les attributs des relations en entrée.
 		 */
 		int i = 0, j = 0;
-		String[] outputSorts = new String[sorts1.length + sorts2.length - 3];
+		String[] outputSorts = new String[sorts1.length + sorts2.length - 1];
 		for(i = 0; i < sorts1.length; i++)
 			outputSorts[i] = sorts1[i];
-		for(j = 0; j < 0; j++, i++)
-			if(j != index2)
-				outputSorts[i] = sorts2[j];
+		for(String str : sorts2){
+			if(j != index)
+				outputSorts[i] = str;
+			i++; j++;
+		}
 
 		String outputRelationName = sgbd.getFreshRelationName();
 		SimpleDBRelation outputRelation = new SimpleDBRelation(new DefaultRelationSchema(outputRelationName, outputSorts));
+		
+		relation1.switchToReadMode();
+		relation2.switchToReadMode();
+		outputRelation.switchToWriteMode();
 		
 		/*
 		 * Copie des tuples dans la relation de sortie.
 		 */
 		for(String[] tuple1 = relation1.nextTuple(); tuple1 != null; tuple1 = relation1.nextTuple()){
 			for(String[] tuple2 = relation2.nextTuple(); tuple2 != null; tuple2 = relation2.nextTuple()){
-				 String[] newTuple = new String[tuple1.length + tuple2.length - 3];
+				 String[] newTuple = new String[tuple1.length + tuple2.length - 1];
 				 for(i = 0; i < tuple1.length; i++)
 					 newTuple[i] = tuple1[i];
 				 for(j = 0; j < tuple2.length; j++, i++)
-					 newTuple[i] = tuple2[j];
+					 if(j != index)
+						 newTuple[i] = tuple2[j];
 				 outputRelation.addTuple(newTuple);
 			}
 		}
+		
+		outputRelation.switchToReadMode();
+		
 		
 		sgbd.addRelation(outputRelationName, outputRelation);
 		
