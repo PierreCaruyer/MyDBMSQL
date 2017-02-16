ff65aaaccc2a259c42b8708264348ba6d32b0efe
frederic.touquet <frederic.touquet@etudiant.univ-lille1.fr>
2017-02-16 17:02:58 +0100

    FT simplification code

--- src/univlille/m1info/abd/phys/ProjectionOperator.java
+++ src/univlille/m1info/abd/phys/ProjectionOperator.java
@@ -1,5 +1,7 @@
 package univlille.m1info.abd.phys;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,40 +14,63 @@ public class ProjectionOperator implements PhysicalOperator{
 	private PhysicalOperator operator;
 	private String[] attributeNames;
 	private SimpleDBRelation relation;
-	private Map<Integer, Integer> attributesMapping;
+//	private Map<Integer, Integer> attributesMapping;
+	private ArrayList<String> attributesMapping;
 	private RelationSchema schema;
 	private int count = 0;
 	
 	public ProjectionOperator(PhysicalOperator operator, String ... attrNames) {
+//		this.operator = operator;
+//		this.attributeNames = attrNames;
+//
+//		attributesMapping = new HashMap<>();
+//		schema = new VolatileRelationSchema(attributeNames);
+//		relation = new SimpleDBRelation(schema);
+//		
+//		String[] sorts = operator.resultSchema().getSort();
+//		
+//		for(int i = 0; i < sorts.length; i++){
+//			if(contains(sorts[i], attrNames)){
+//				this.attributeNames[i] = sorts[i];
+//				attributesMapping.put(count, i);
+//				count++;
+//			}
+//		}
 		this.operator = operator;
 		this.attributeNames = attrNames;
-
-		attributesMapping = new HashMap<>();
-		schema = new VolatileRelationSchema(attributeNames);
+		
+		schema = new VolatileRelationSchema(attrNames);
 		relation = new SimpleDBRelation(schema);
 		
 		String[] sorts = operator.resultSchema().getSort();
 		
-		for(int i = 0; i < sorts.length; i++){
-			if(contains(sorts[i], attrNames)){
-				this.attributeNames[i] = sorts[i];
-				attributesMapping.put(count, i);
-				count++;
-			}
+		for ( String s : sorts ) {
+			if ( Arrays.asList(attrNames).contains(s) )
+				attributesMapping.add(s);
 		}
 	}
 	
 	@Override
 	public String[] nextTuple() {
- 		String[] tuple = new String[attributeNames.length], currentTuple = operator.nextTuple();
- 		
- 		if(currentTuple == null)
- 			return null;
- 		
- 		for(int i = 0; i < count; i++)
-			tuple[i] = currentTuple[attributesMapping.get(i)];
- 		
- 		return tuple;
+// 		String[] tuple = new String[attributeNames.length], currentTuple = operator.nextTuple();
+// 		
+// 		if(currentTuple == null)
+// 			return null;
+// 		
+// 		for(int i = 0; i < count; i++)
+//			tuple[i] = currentTuple[attributesMapping.get(i)];
+// 		
+// 		return tuple;
+ 		ArrayList<String> tuple = new ArrayList<String>();
+		String[] currentTuple = operator.nextTuple();
+		
+		if ( currentTuple == null ) return null;
+		
+		for ( String c : currentTuple ) {
+			tuple.add(c);
+		}
+		
+		return tuple.toArray(new String[tuple.size()]);
 	}
 
 	@Override
@@ -58,10 +83,10 @@ public class ProjectionOperator implements PhysicalOperator{
 		relation.switchToReadMode();
 	}
 	
-	private boolean contains(String str, String[] array){
-		for(String string : array)
-			if(str.equals(string))
-				return true;
-		return false;
-	}
+//	private boolean contains(String str, String[] array){
+//		for(String string : array)
+//			if(str.equals(string))
+//				return true;
+//		return false;
+//	}
 }
